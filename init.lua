0ef2b624aa2d491ee03bb2db772155f472f6533e
kngpn <akulayash1996@gmail.com>
2025-07-24 15:47:59 -0700

    added nvim-treesitter

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("lazy").setup({
+  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
+})

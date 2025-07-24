a6ede38f3f347761c80754841009b9e31ae8c0f7
kngpn <akulayash1996@gmail.com>
2025-07-24 16:10:30 -0700

    config change to prevent toggle term from firing when editing text files

--- lua/plugins/toogleterm.lua
+++ lua/plugins/toogleterm.lua
@@ -5,8 +5,12 @@ return {
     config = function()
       require("toggleterm").setup{
         size = 10,
-        open_mapping = [[<leader>t]],  -- keybinding to toggle terminal
-        direction = "horizontal",      -- terminal opens in horizontal split
+        open_mapping = [[<leader>t]],  -- toggle only on normal mode with <leader>t
+        direction = "horizontal",
+        start_in_insert = true,
+        insert_mappings = false,       -- disable insert mode mappings to avoid triggering on typing
+        terminal_mappings = false,     -- disable terminal mode mappings
+        close_on_exit = true,
       }
     end,
   },

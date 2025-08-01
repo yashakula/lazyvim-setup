7fc51af3ce3ed6edfe344a779489a3aa659bbc85
kngpn <akulayash1996@gmail.com>
2025-08-01 09:17:14 -0700

    added options for text wrap around

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Text wrapping options
+vim.opt.wrap = true          -- Enable line wrapping
+vim.opt.linebreak = true     -- Break lines at word boundaries
+vim.opt.showbreak = "↪ "     -- String to show at the start of wrapped lines

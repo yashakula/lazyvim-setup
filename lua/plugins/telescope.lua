7564594219e03b537dd0c80e3325d0cb8beb3b7e
Yash Akula <akulayash1996@gmail.com>
2025-07-31 17:06:55 -0700

    changes

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,49 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  cmd = "Telescope",
+  version = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      enabled = function()
+        return vim.fn.executable("make") == 1
+      end,
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+    { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent Files" },
+    { "<leader>fc", "<cmd>Telescope git_commits<cr>", desc = "Git Commits" },
+    { "<leader>fs", "<cmd>Telescope git_status<cr>", desc = "Git Status" },
+    { "<leader>fw", "<cmd>Telescope grep_string<cr>", desc = "Grep String" },
+  },
+  opts = function()
+    return {
+      defaults = {
+        prompt_prefix = " ",
+        selection_caret = " ",
+        mappings = {
+          i = {
+            ["<C-n>"] = require("telescope.actions").move_selection_next,
+            ["<C-p>"] = require("telescope.actions").move_selection_previous,
+            ["<C-j>"] = require("telescope.actions").move_selection_next,
+            ["<C-k>"] = require("telescope.actions").move_selection_previous,
+          },
+        },
+      },
+      pickers = {
+        find_files = {
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
+      },
+    }
+  end,
+}
